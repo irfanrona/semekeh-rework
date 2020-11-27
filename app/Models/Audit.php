15ae964737e9a0a78f1509ce65beb057e9aa8c6a
Muhammad Dafitra Ar-Rasyid <muhamad.odd@gmail.com>
2020-11-27 15:17:15 +0700

    Updated created at format

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -9,6 +9,15 @@ class Audit extends Model
     public static function getAll(){
         return self::leftJoin('users', 'users.id', '=', 'user_id')
             ->orderBy('audits.created_at', 'desc')
-            ->get();
+            ->get([
+            	'name',
+            	'auditable_type',
+            	'event',
+            	'audits.created_at',
+            	'ip_address',
+            	'user_agent',
+            	'old_values',
+            	'new_values',
+            ]);
     }
 }
